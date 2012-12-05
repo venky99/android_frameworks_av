940fd9d8fbd536674420cef66a9a878772731308
vivek mehta <mvivek@codeaurora.org>
2012-12-04 20:57:49 -0800

    libstagefright: Add support to seek to zero
    - Add CAN_SEEK_TO_ZERO flag in MediaExtractor to allow
      seek to zero, when previous button is pressed, for clip
      that can't be seeked.
    
    Change-Id: Ic2761cc5bac2403ee1339da77ad02fde942a3437
    
    Conflicts:
    	media/libstagefright/AwesomePlayer.cpp

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1,9 +1,7 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
- * Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
- * Not a Contribution, Apache license notifications and license are retained
- * for attribution purposes only.
-
+ * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
