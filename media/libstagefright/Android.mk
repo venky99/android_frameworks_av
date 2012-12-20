e4a6d328d40dfa46f25ef4bbb183579c2749e3b0
Erik Casticum <erikcas1972@gmail.com>
2012-12-20 10:59:53 -0500

    Fixes for the OMX hack
    Fixes the lag and adds some lost code
    
    Change-Id: I733258f13bad124956aa95b5503d1e1e57996840
    
    Conflicts:
    	media/libstagefright/Android.mk
    	media/libstagefright/OMXCodec.cpp
    
    Conflicts:
    	media/libstagefright/OMXCodec.cpp

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -7,6 +7,33 @@ ifeq ($(BOARD_USES_ALSA_AUDIO),true)
         LOCAL_CFLAGS += -DTUNNEL_MODE_SUPPORTS_AMRWB
     endif
 endif
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+    LOCAL_CFLAGS += -DUSE_AAC_HW_DEC
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
+    LOCAL_CFLAGS += -DTARGET7x27
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+    LOCAL_CFLAGS += -DTARGET7x27A
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+    LOCAL_CFLAGS += -DTARGET7x30
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    LOCAL_CFLAGS += -DTARGET8x50
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+    LOCAL_CFLAGS += -DTARGET8x60
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+    LOCAL_CFLAGS += -DTARGET8x60
+endif
+ifeq ($(BOARD_CAMERA_USE_MM_HEAP),true)
+    LOCAL_CFLAGS += -DCAMERA_MM_HEAP
+endif
+endif
 
 include frameworks/av/media/libstagefright/codecs/common/Config.mk
 
@@ -75,7 +102,8 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/external/flac/include \
         $(TOP)/external/tremolo \
-        $(TOP)/external/openssl/include
+        $(TOP)/external/openssl/include \
+        $(TOP)/hardware/qcom/display/libqdutils
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 LOCAL_SRC_FILES += \
@@ -96,7 +124,6 @@ LOCAL_SRC_FILES += \
 endif
 endif
 
-
 LOCAL_SHARED_LIBRARIES := \
         libbinder \
         libcamera_client \
@@ -111,6 +138,7 @@ LOCAL_SHARED_LIBRARIES := \
         liblog \
         libmedia \
         libmedia_native \
+        libqdutils \
         libsonivox \
         libssl \
         libstagefright_omx \
